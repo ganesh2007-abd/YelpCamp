3d7a1b486e34b5b048e1025cdd6d0c023d89bdb4
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-24 13:51:52 +0530

    added sample campground model

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const ejs = require('ejs')
 const path = require('path')
+const mongoose = require('mongoose')
+const Campground = require('./models/campground')
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/FarmStand')
+    .then(() => {
+        console.log("Mongodb Connection Open!")
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error")
+        console.log(err)
+    })
+
 
 const app = express()
 
@@ -9,6 +22,12 @@ app.set('view engine', ejs)
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
+})
+
+app.get('/makecampground', async (req, res) => {
+    const Camp = new Campground({ title: "My backyard", price: "26", description: "very cheap" })
+    await Camp.save()
+    res.send(Camp)
 })
 
 app.listen(3000, () => {
