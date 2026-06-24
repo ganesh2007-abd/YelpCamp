db8ba43215e324f62c14e2f5ce4dc265ead36960
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-24 18:34:34 +0530

    added delete route

--- app.js
+++ app.js
@@ -73,7 +73,11 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
-
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
 
 
 app.listen(3000, () => {
