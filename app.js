11ad660d2a6f971da85d8ede9f8d41e65360412d
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-24 17:24:38 +0530

    added show route

--- app.js
+++ app.js
@@ -35,6 +35,12 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index.ejs', { campgrounds })
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/show.ejs', { campground })
+})
+
 app.listen(3000, () => {
     console.log("Listening on 3000...")
 })
