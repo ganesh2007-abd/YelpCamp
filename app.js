ee134d998c3ca8c7f681be124b97e8c902366ae1
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-07-09 01:15:28 +0530

    just serving static files

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/campgrounds', Campgroundroutes)
 app.use('/campgrounds/:id/reviews', Reviewroutes)
