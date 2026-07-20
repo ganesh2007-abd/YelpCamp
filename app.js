2ff922f419ca0a3ae0b8ba866f28b92b25a09812
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-07-20 12:56:36 +0530

    showing and hiding buttons using req.user object

--- app.js
+++ app.js
@@ -60,19 +60,23 @@ const sessionConfig = {
 
 app.use(session(sessionConfig))
 
-app.use(flash())
-app.use((req, res, next) => {
-    res.locals.success = req.flash('success')
-    res.locals.error = req.flash('error')
-    next()
-})
-
 app.use(passport.initialize())
 app.use(passport.session())
 
 passport.use(new localStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
+
+
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.currUser = req.user
+    console.log(req.user)
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
+})
 
 app.use('/campgrounds', Campgroundroutes)
 app.use('/campgrounds/:id/reviews', Reviewroutes)
