261c84e21a8c6008f4509ebf38b52c4f4074a138
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-07-26 12:59:05 +0530

    comoletly wrppaed up

--- seeds/index.js
+++ seeds/index.js
@@ -18,33 +18,40 @@ const sample = (array) => {
 }
 
 const seedDB = async () => {
-    await Campground.deleteMany({})
+    await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000)
-        const price = Math.floor(Math.random() * 4000) + 200
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            location: `${cities[random1000].city},${cities[random1000].state}`,
+
+            author: '',
+
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            // image: `https://picsum.photos/400?random=${Math.random()}`,
-            description: "Very good place",
-            author: '6a5ddb863c419404902eb17d',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
+
             images: [
                 {
-                    url: 'https://res.cloudinary.com/sgzwlhoq/image/upload/v1784886576/YelpCamp/lb3kpxbdqxkqprfaofqx.png',
-                    filename: 'YelpCamp/lb3kpxbdqxkqprfaofqx',
-
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
                 },
                 {
-                    url: 'https://res.cloudinary.com/sgzwlhoq/image/upload/v1784886575/YelpCamp/xybxta9zxq6orn7eznpo.png',
-                    filename: 'YelpCamp/xybxta9zxq6orn7eznpo',
-
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
                 }
             ]
         })
-        await camp.save()
+        await camp.save();
     }
-
 }
 
 seedDB().then(() => {
